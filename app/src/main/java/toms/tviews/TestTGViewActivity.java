91db81ee93e72c517cc4c2f5bd872fda775221ae
tsottili <tommaso.sottili@gmail.com>
2015-10-05 23:41:33 +0200

    add empty Test activity for GView

---
+++ app/src/main/java/toms/tviews/TestTGViewActivity.java
@@ -0,0 +1,13 @@
+package toms.tviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestTGViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_tgview);
+    }
+}
